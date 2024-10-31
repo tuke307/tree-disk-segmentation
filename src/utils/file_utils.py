675da568569854f7102f1460112e0ffb16287071
Tony <tonymeissner70@gmail.com>
2024-10-31 22:57:50 +0100

    feat: Add utility functions

---
+++ src/utils/file_utils.py
@@ -0,0 +1,90 @@
+import os
+import json
+from pathlib import Path
+import cv2
+import shutil
+from pathlib import Path
+from typing import List, Any, Tuple, Dict
+import numpy as np
+
+
+def load_image(filename: str) -> np.ndarray:
+    """
+    Load image utility.
+
+    Args:
+        filename (str): Path to image file.
+
+    Returns:
+        np.ndarray: Image as a numpy array.
+    """
+    img = cv2.imread(filename)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    return img
+
+
+def save_image(filename: str, result_image: np.ndarray) -> None:
+    """
+    Save the final image after removing the salient object.
+
+    Args:
+        filename (str): Path to save the output image.
+        result_image (np.ndarray): The result image with the background set to white.
+
+    Returns:
+        None
+    """
+    # Convert to BGR if needed
+    result_image_bgr = cv2.cvtColor(result_image, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(filename, result_image_bgr)
+
+
+def load_json(filepath: str) -> dict:
+    """
+    Load json file utility.
+
+    Args:
+        filepath (str): path to json file
+
+    Returns:
+        dict: json file as a dictionary
+    """
+    with open(str(filepath), "r") as f:
+        data = json.load(f)
+
+    return data
+
+
+def write_json(dict_to_save: dict, filepath: str) -> None:
+    """
+    Write json file utility.
+
+    Args:
+        dict_to_save (dict): dictionary to save
+        filepath (str): path to save json file
+
+    Returns:
+        None
+    """
+    with open(str(filepath), "w") as f:
+        json.dump(dict_to_save, f)
+
+
+def clear_dir(dir: str) -> None:
+    """
+    Clear directory utility.
+
+    Args:
+        dir (str): directory to clear
+
+    Returns:
+        None
+    """
+    dir = Path(dir)
+
+    for item in dir.iterdir():
+        if item.is_file():
+            item.unlink()
+        elif item.is_dir():
+            shutil.rmtree(item)
